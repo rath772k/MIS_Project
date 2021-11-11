257a07145a5f15fba83a2ebf016b1552d77396e4
rath772k <45796608+rath772k@users.noreply.github.com>
2021-11-11 21:10:06 +0530

    Update report_model.php

--- models/fee_structure/report_model.php
+++ models/fee_structure/report_model.php
@@ -1,29 +1,63 @@
 <?php
 class Report_model extends CI_Model
 {
+    public $numeric;
     function __construct() {
         // Call the Model constructor
         parent::__construct();
+        $this->numeric = array(
+                "tution_fees","annual_charge","medical_fund",
+                "sports_subscription_fee","house_rent",
+                "semester_registration_fee","examination_fee",
+                "computer_and_internet_charges","electricity_charges",
+                "library_fee","training_and_placement_support_fee",
+                "miscellaneous_fee");
     }
 
     function getAllRows(){
+        $sum_part = "";
+        foreach($this->numeric as $column)
+        {
+            $sum_part .= "b.".$column."+";
+        }
+        $sum_part .="a.late_fine 
+                    + a.pending_amount 
+                    - a.refundable_amount
+                    AS total_fee";
+
         $query ="
-                SELECT a.*,b.* FROM stu_details_project AS a 
+                SELECT a.*,"
+                .$sum_part
+                .",b.* FROM stu_details_project AS a 
                 JOIN
                 stu_fee_structure AS b
-                ON a.session_year = b.session_year AND a.session = b.session AND a.category = b.category 
-                AND b.is_deleted='NO';
-                ";
+                ON a.session_year = b.session_year AND a.session = b.session AND
+                (a.category = b.category OR (b.category = 'PWD' AND a.pwd_status = 'YES')) 
+                AND  b.is_deleted='NO';";
         $rows = $this->db->query($query);
         return $rows->result_array();
     }
 
     function getRequiredRows($session_year, $session){
+        
+        $sum_part = "";
+        foreach($this->numeric as $column)
+        {
+            $sum_part .= "b.".$column."+";
+        }
+        $sum_part .="a.late_fine 
+                    + a.pending_amount 
+                    - a.refundable_amount
+                    AS total_fee";
+
         $query ="
-                SELECT a.*,b.* FROM stu_details_project AS a 
+                SELECT a.*,"
+                .$sum_part
+                .",b.* FROM stu_details_project AS a 
                 JOIN
                 stu_fee_structure AS b
-                ON a.session_year = b.session_year AND a.session = b.session AND a.category = b.category 
+                ON a.session_year = b.session_year AND a.session = b.session AND
+                (a.category = b.category OR (b.category = 'PWD' AND a.pwd_status = 'YES')) 
                 AND a.session_year='".$session_year
                 ."' AND a.session='".$session."' AND b.is_deleted='NO';";
 
